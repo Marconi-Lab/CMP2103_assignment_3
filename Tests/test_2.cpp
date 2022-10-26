d39057ce3b9998902d11d9851a3d70f7d2ceabcd
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-10-26 22:09:35 +0300

    Update test_2.cpp

--- Tests/test_2.cpp
+++ Tests/test_2.cpp
@@ -6,6 +6,6 @@ float a4 = (10/50.4)*pow(2.71828,-2.11/(50.4*0.16));
 float a5 = (35/50.0)*pow(2.71828,-0.11/(50.0*0.016));
 TEST_CASE( "Tests_2", "Tests_2" ) {
     REQUIRE(abs(RC_current(10,50.4,0.16,2.11) - a4)<1e-6);
-    REQUIRE( abs(RC_current(35,50,0.016,0.11) - a5)<1e-6);
+    REQUIRE(abs(RC_current(35,50,0.016,0.11) - a5)<1e-6);
 }
 
