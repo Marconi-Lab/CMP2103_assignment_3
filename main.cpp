a1e20d172f8fc7109815eb15e28385c65b6f04a7
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-10-26 22:13:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,12 @@
 // Choose proper variable names
 // Remember to define variables with the right data types
 using namespace std;
-float RC_current( float E, float R, float C, float t ) {
-    const float e = 2.71828;
-    float current_0 = E/R;
-    float time_constant = -t/(R*C);
-    float expon = pow(e,time_constant);
-    float I = current_0 * expon;
+float RC_current(//define variables here) {
   // Put your code here
-  return I;
+  return ;
 }
 
 //int main(){
-//cout<<RC_current(6,100,0.026,1.4)<<endl;
-//cout<<RC_current(15,20,0.055,0.067)<<endl;
-//cout<<RC_current(35,50,0.016,0.11)<<endl;
-//cout<<RC_current(5,1,0.016,0.11)<<endl;
-//cout<<RC_current(10,50.4,0.16,2.11)<<endl;
+    //Carry out some tests here and comment this section out when submitting
 //}
 
